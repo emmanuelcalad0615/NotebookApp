10d93be1e0e1b4bd0b7dee0407f36832187954c0
Emmanuel Calad <emmanuelcalad@gmail.com>
2024-04-03 18:22:50 -0500

    add class Note and class Notebook

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,1 +1,39 @@
 from datetime import datetime
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Note:
+    code: int
+    title: str
+    text: str
+    importance: str
+    creation_date: datetime = field(default_factory=datetime. now())
+    tags: list[str] = field(default_factory=list[str])
+
+    def __str__(self):
+        return f"Code: {self.code}" \
+               f"Creation date: {self.creation_date}" \
+               f"Title: {self.text}"
+
+    def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+
+class Notebook:
+    def __init__(self, notes: list[Note]):
+        self.notes: list[Note] = notes
+
+    def add_note(self, title: str, text: str, importance: str):
+        code_id = len(self.notes)
+        nota = Note(code_id, title, text, importance)
+        self.notes.append(nota)
+
+    def important_notes(self) -> list[Note]:
+        important_list: list[Note] = [important for important in self.notes if important.importance == "HIGH" or important.importance == "MEDIUM"]
+        return important_list
+
+    def tags_note_count(self) -> dict[str, int]:
+        pass
+
